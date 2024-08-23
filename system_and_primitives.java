b53c718da08abaa1472c325f59871dee2eb95ffe
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-08-23 13:13:58 -1000

    Create system_and_primitives.java

---
+++ system_and_primitives.java
@@ -0,0 +1,56 @@
+package Systems_and_Primitives;
+
+public class Systems_and_Primitives {
+
+	public static void main(String[] args) {
+		//task 1: print out my name "Keola" in ascii art
+		System.out.println("o   o   o o o     o o o     o             o");
+		System.out.println("o  o    o        o     o    o            o o");
+		System.out.println("o o     o o     o       o   o           o   o");
+		System.out.println("o  o    o        o     o    o          o o o o");
+		System.out.println("o   o   o o o     o o o     o o o o   o       o");
+		System.out.println("is literally the best" + ". Watch this.");
+		System.out.println(" ");
+		
+		
+		//task 2: Solve a math problem
+		double prod1 = 6.0 * 3.5;
+		double prod2 = 1.5 * 5;
+		double dif1 = prod1 - prod2;
+		double dif2 = 55.6 - 30.2;
+		double ans = dif1 / dif2;
+		System.out.println(ans);
+		System.out.println("You see that? Now watch this");
+		System.out.println(" ");
+		
+	
+		//task 3: White adaptable code to print out the C and A of a circle
+		final double pi = 3.1519;
+		double rad = 4.0;
+		double circ = 2 * pi * rad;
+		double expo = rad * rad;
+		double area = pi * expo;
+		System.out.println(circ);
+		System.out.println(area);
+		System.out.println(" ");
+		
+		
+		//task 4: glaze myself
+		System.out.print("Yeah I know, I'm the best");
+		
+		
+		
+		
+
+	}
+
+}
+
+
+/*
+ * o   o   o o o     o o o     o             o
+ * o  o    o        o     o    o            o o
+ * o o     o o     o       o   o           o   o
+ * o  o    o        o     o    o          o o o o
+ * o   o   o o o     o o o     o o o o   o       o
+ */
