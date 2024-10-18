06661bfef07f273ca46851a8ed8e23180ae60133
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-10-18 13:42:06 -1000

    Update LogicCodingbat1.java

--- CodingBatSolns/LogicCodingbat1.java
+++ CodingBatSolns/LogicCodingbat1.java
@@ -3,7 +3,7 @@ Task 1:
 */
 public boolean cigarParty(int cigars, boolean isWeekend)
 {
-	if (isWeekend == true)
+if (isWeekend == true)
 	{
 	  return (cigars >= 40? true: false);
 	}
@@ -19,19 +19,14 @@ Task 2:
 */
 public int dateFashion(int you, int date)
 {
-  //if statement to check is you or date is less than 2 (inclusive)
   if (you <= 2 || date <= 2)
   {
-    //returns 0
     return 0;
   }
-  //else if statement to check if you or date is greater than 8 (inclusive)
   else if (you >= 8 || date >= 8)
   {
-    //returns 2
     return 2;
   }
-  //returns 1 if every check is false
   return 1;
 }
 
@@ -50,42 +45,15 @@ public boolean squirrelPlay(int temp, boolean isSummer) {
 Task 4:
 */
 public int caughtSpeeding(int speed, boolean isBirthday) {
-  //if statement to check if isBirthday true
-  if (isBirthday == true)
+  if (isBirthday == true && speed > 65)
   {
-    //if statement to check if speed is less than 65 (inclusive)
-    if (speed <= 65)
-    {
-      //returns 0
-      return 0;
-    }
-    //if statment to check if speed is greater than 85
-    else if (speed > 85)
-    {
-      //returns 2
-      return 2;
-    }
-    //else statement for if every other check was false
-    else
-    {
-      //returns 1
-      return 1;
-    }
+    return speed <= 85? 1 : 2;
   }
-  //if statement to check if speed is less than 60 (inclusive)
-  if (speed <= 60)
+  if (isBirthday == false && speed > 60)
   {
-    //returns 0
-    return 0;
+    return speed <= 80? 1 : 2;
   }
-  //else if statement to check if speed is greater than 80
-  else if (speed > 80)
-  {
-    //returns 2
-    return 2;
-  }
-  //returns 1 if every other check was false
-  return 1;
+  return 0;
 }
 
 /**
@@ -93,14 +61,10 @@ Task 5:
 */
 public int sortaSum(int a, int b)
 {
-  //int to calculate a + b
   int sum = a + b;
-  //if statement to check if sum is between 10 and 19 (inclusive)
   if (sum >= 10 && sum <= 19)
   {
-    //returns 20
     return 20;
   }
-  //returns the calculated sum if check was false
   return sum;
 }
