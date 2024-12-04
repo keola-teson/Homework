ceb7620b504b35981661028c25f3a4ef1c21ee67
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-12-03 21:33:08 -1000

    Update StringPractice.java

--- StringPractice.java
+++ StringPractice.java
@@ -1,71 +1,71 @@
-import java.util.Scanner;
+//assume circle object and scanner were instantiated elsewhere
 
-public class StringPractice
+/**
+ * This method checks for if the user inputted stop and stops the loop
+ */
+public static void stopMe()
 {
-	public static void main(String[] args)
+	//declares iterations int variable
+	int iterations = 0;
+	//String to change
+	String stopInput = "";
+	//checks if the player inputted "stop"
+	while (!(stopInput.equalsIgnoreCase("stop")))
 	{
-		//circle object with radius value 4.0
-		Circle circle = new Circle(4.0);
-		//scanner input
-		Scanner scanner = new Scanner(System.in);
-		
-		//declares iterations int variable
-		int iterations = 0;
-		//String to change
-		String stopInput = "";
-		//checks if the player inputted "stop"
-		while (!(stopInput.equalsIgnoreCase("stop")))
-		{
-			System.out.print("Input text: ");
-			//allows player to type
-			stopInput = scanner.nextLine();
-			//adds one to iterations
-			iterations++;
-			/*
-			 * checks if the user pressed enter
-			 * - prints the information and continues the loop skipping everything under it
-			 * 
-			 * checks if the player only inputted a space
-			 * - prints the information and continues the loop skipping everything under it
-			 * 
-			 * prints the information and continues the loop
-			 */
-			if (stopInput.length() == 0)
-			{
-				System.out.println("\nText Inputted: [Nothing]");
-				System.out.println("Iteration: " + iterations);
-				continue;
-			}
-			if (stopInput.length() == 1 && stopInput.charAt(0) == ' ')
-			{
-				System.out.println("\nText Inputted: [Space]");
-				System.out.println("Iteration: " + iterations);
-				continue;
-			}
-			System.out.println("Text inputted: \"" + stopInput + "\"");
-			System.out.println("Iteration: " + iterations + "\n");
-		}
-		//prints the amount of times the loop what iterated through
-		System.out.println("Iterations: " + iterations);
-		System.out.println("");
-		
-		//declares a double with the value of the circle object's radius
-		double circleRadius = circle.getRadius();
+		System.out.print("Input text: ");
+		//allows player to type
+		stopInput = scanner.nextLine();
+		//adds one to iterations
+		iterations++;
 		/*
-		 * loops 5 times
-		 * - allows the player to input a number and adds that number to the double variable
+		 * checks if the user pressed enter
+		 * - prints the information and continues the loop skipping everything under it
+		 * 
+		 * checks if the player only inputted a space
+		 * - prints the information and continues the loop skipping everything under it
+		 * 
+		 * prints the information and continues the loop
 		 */
-		for (int i = 1; i <= 5; i++)
+		if (stopInput.length() == 0)
 		{
-			System.out.print("Input a number: ");
-			double circleInput = scanner.nextDouble();
-			
-			circleRadius += circleInput;
+			System.out.println("\nText Inputted: [Nothing]");
+			System.out.println("Iteration: " + iterations);
+			continue;
 		}
-		//prints the modified circle radius
-		System.out.println("Radius of the circle is " + circleRadius);
-		
-		//closes scanner
-		scanner.close();
+		if (stopInput.length() == 1 && stopInput.charAt(0) == ' ')
+		{
+			System.out.println("\nText Inputted: [Space]");
+			System.out.println("Iteration: " + iterations);
+			continue;
+		}
+		System.out.println("Text inputted: \"" + stopInput + "\"");
+		System.out.println("Iteration: " + iterations + "\n");
 	}
+	//prints the amount of times the loop what iterated through
+	System.out.println("Iterations: " + iterations);
+}
+
+/**
+ * This method modifies the radius of the circle object instantiated as a data attribute
+ */
+public static void changeRadius()
+{
+	//declares a double with the value of the circle object's radius
+	double circleRadius = circle.getRadius();
+	/*
+	 * loops 5 times
+	 * - allows the player to input a number and adds that number to the double variable
+	 */
+	for (int i = 1; i <= 5; i++)
+	{
+		System.out.print("Input a number: ");
+		double circleInput = scanner.nextDouble();
+		
+		circleRadius += circleInput;
+	}
+	//sets radius to the modified value of circleRadius
+	circle.setRadius(circleRadius);
+	
+	//prints the modified circle radius
+	System.out.println("Radius of the circle is " + circle.getRadius());
 }
